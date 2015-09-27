757bfe1f1a5cdec8b081878f8baefcd352eed761
Techcable <Techcable@outlook.com>
2015-09-26 19:29:36 -0700

    Retain settings at runtime

--- src/main/java/net/techcable/phanatic/ftweaks/config/Setting.java
+++ src/main/java/net/techcable/phanatic/ftweaks/config/Setting.java
@@ -1,5 +1,9 @@
 package net.techcable.phanatic.ftweaks.config;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Setting {
     public String value();
 }
